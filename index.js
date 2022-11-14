27358e1f9e5f7c5287546d77b6612c245b4aa9a3
koz <kozplay11@gmail.com>
2022-11-13 19:10:35 -0600

    ping to keep alive

--- index.js
+++ index.js
@@ -179,4 +179,6 @@ server.on('upgrade', function upgrade(request, socket, head) {
 
 server.on('request', app);
 
+setInterval(function(){hqSocket.ping();}, 3000);
+
 server.listen(PORT, () => console.log(`Listening on ${ PORT }`))
